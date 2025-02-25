99ba3031d1db2bcca331d622a9a2d80d06b0dd20
Caner Mollaoğlu <canermollaoglu@hotmail.com>
2025-02-25 15:06:58 +0300

    Form load mesaj kutusu eklendi.

--- Form1.cs
+++ Form1.cs
@@ -6,5 +6,10 @@ namespace EcommerceProject
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show("Test");
+        }
     }
 }
